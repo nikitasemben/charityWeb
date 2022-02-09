5073b5dbdcbf6ba494d271e5e00b469b5ba6af46
NikitaSemben\lenovo <71478762+nikitasemben@users.noreply.github.com>
2022-02-09 21:02:53 +0700

    Charity about-contact

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,21 @@
 <script>
+	import router from 'page';
 	import Home from './pages/Home.svelte';
+	import About from './pages/About.svelte';
+	import Contact from './pages/contact.svelte';
+	import Donation from './pages/Donation.svelte';
+
+	let page, params;
+	router('/', ()=> (page = Home));
+	router('/about', ()=> (page = About));
+	router('/contact', ()=> (page = Contact));
+	router('/donation/:id', (ctx, next)=>{
+		params = ctx.params;
+		next();
+	},()=>(page=Donation));
+
+	router.start();
+
 </script>
 
-<Home />
+<svelte:component this={page} {params} />
