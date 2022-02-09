aaeec1aa60479ded29994f53bcdb08b8f76536c6
NikitaSemben\lenovo <71478762+nikitasemben@users.noreply.github.com>
2022-02-09 13:32:12 +0700

    Charity web

---
+++ src/data/charities.js
@@ -0,0 +1,16 @@
+module.exports = {
+    charities: [
+        {
+            id: 1,
+            title: 'First Charity Project',
+            category: 'Money',
+            thumbnail: 'https://image.freepik.com/free-vector/people-carrying-donation-charity-related-icons_53876-43091.jpg',
+            pledged: 0,
+            target: 500000,
+            date_end: +new Date('15 August 2020'),
+            profile_photo: 'https://filmunik.com/wp-content/uploads/2021/07/tanjiro-kamado-tu-husbando-ideal_optimized.png',
+            profile_name: 'Aya',
+            no_pledgets: 0
+        }
+    ]
+}
---
+++ src/pages/Home.svelte
@@ -0,0 +1,16 @@
+<script>
+    import CharityList from '../components/charityList.svelte';
+    import Header from '../components/Header.svelte';
+    import Welcome from '../components/welcome.svelte';
+    import Promo from '../components/promo.svelte';
+    import Footer from '../components/Footer.svelte';
+    import { charities } from '../data/charities.js';
+	let title = "Charity";
+</script>
+
+<Header />
+<Welcome />
+<CharityList {charities}/>
+<Promo />
+<Footer />
+    
